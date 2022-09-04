420bab160f02a22b52857de59b1744584c39f231
Vadim Shakirov <shakirov@ispras.ru>
2022-09-04 23:47:51 +0300

    add variable

--- graph.h
+++ graph.h
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <exception>
 #include <stdexcept>
+#include <tuple>
 
 namespace Graph {
 
@@ -22,32 +23,30 @@ struct just_exist_vertex : std::exception {
 	}
 };
 
-template <typename T>
+template <typename ...types>
 struct vertex_data final {
 
-	T attr; // must be copied?
+	std::tuple<types...> attrs;
 	std::list<int> adjacency_list;
-
-	vertex_data(const T& attr_) : attr(attr_) {
-		std::cout << "ctor vertex_data" << std::endl;
-	}
+				// But best forwarding???
+	vertex_data(const types&... attrs_) : attrs(make_tuple(attrs_...)) {}
 };
 
-template<typename T>
+template<typename ...types>
 class graph final {
 
-	using list_it = typename std::list<vertex_data<T>>::iterator;
+	using list_it = typename std::list<vertex_data<types...>>::iterator;
 
-	std::list<vertex_data<T>> vertex_list;
+	std::list<vertex_data<types...>> vertex_list;
 	std::map<int, list_it> vertex_map;
 
 public:
 
 	graph() {}
 							
-	void add_vertex(int num, const T& attr) {
-
-		vertex_list.push_back(vertex_data<T>(attr));
+	void add_vertex(int num, types&&... attrs) {
+							// copy?
+		vertex_list.emplace_back(vertex_data<types...>(attrs...));
 		auto ret = vertex_map.emplace(num, --vertex_list.end());
 		if (ret.second == false) throw just_exist_vertex();
 	}
@@ -86,11 +85,31 @@ public:
 		// And what if end not in a graph?
 	}
 
+	void print_tuple(const std::tuple<types...>& tpl) {
+
+	    std::cout << "(";
+	    std::apply([](const auto&... tpl_args) {
+
+	                size_t index = 0;
+	                auto print_elem = [&index](const auto& part) {
+	                    if (index > 0) std::cout << ", ";
+	                    index++;
+	                    std::cout << part;
+	                };
+
+	                (print_elem(tpl_args), ...);
+
+	            }, tpl);
+	    std::cout << ")";
+	}
+
 	void dump() noexcept {
 // no auto&& because there is value not ref
 		for (auto vertex : vertex_map) {
 
-			std::cout << vertex.first << " [" << vertex.second->attr << "] : ";
+			std::cout << vertex.first << " ["; 
+			print_tuple(vertex.second->attrs);
+			std::cout << "] : ";
 
 			for (auto it : vertex.second->adjacency_list) {
 
--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 
 int main() {
 
-	Graph::graph<std::string> gr;
+	Graph::graph<std::string, int> gr;
 
 	std::string forth("forth_and_two");
 
-	gr.add_vertex(1, "first");
-	gr.add_vertex(3, "second");
-	gr.add_vertex(10, "third");
-	gr.add_vertex(12, forth.substr(5));
+	gr.add_vertex(1, "first", 1);
+	gr.add_vertex(3, "second", 2);
+	gr.add_vertex(10, "third", 3);
+	gr.add_vertex(12, forth.substr(5), 4);
 
 	gr.add_edge(1, 3);
 	gr.add_edge(3, 10);
